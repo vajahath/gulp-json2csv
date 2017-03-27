f60e1fc51100da143b297ff9e263d86a0cbad259
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-27 15:34:08 +0530

    getting things ready

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,1 +1,53 @@
+/*global describe it*/
 
+var assert = require('assert');
+var File = require('vinyl');
+var converter = require('../');
+
+describe('gulp-json2csv', function() {
+	describe('in buffer mode', function() {
+
+		it('should successfully convert to csv', function(done) {
+			// create the fake file
+			var fakeFile = new File({
+				contents: new Buffer('' +
+					'[' +
+					'{' +
+					'"car": "Audi",' +
+					'"price": 40000,' +
+					'"color": "blue"' +
+					'}, {' +
+					'"car": "BMW",' +
+					'"price": 35000,' +
+					'"color": "black"' +
+					'}, {' +
+					'"car": "Porsche",' +
+					'"price": 60000,' +
+					'"color": "green"' +
+					'}' +
+					']')
+			});
+
+			// Create a converter plugin stream
+			var converted = converter(fakeFile);
+
+			// write the fake file to it
+			converted.write(fakeFile);
+
+			// wait for the file to come back out
+			converted.once('data', function(file) {
+				// make sure it came out the same way it went in
+				assert(file.isBuffer());
+
+				// check the contents
+				assert.equal(file.contents.toString('utf8'), '"car","price","color"\n' +
+					'"Audi",40000,"blue"\n' +
+					'"BMW",35000,"black"\n' +
+					'"Porsche",60000,"green"');
+				done();
+			});
+
+		});
+
+	});
+});
